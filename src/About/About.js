55f479566319793092ed9c3d7e9ba5c7494bcb8b
Woody Kromar <wkromar12@gmail.com>
2021-02-28 17:19:40 -0600

    trying to get modals to work

--- src/About/About.js
+++ src/About/About.js
@@ -6,18 +6,18 @@ function About() {
     <div className="about">
       <h2>About</h2>
       <div className="aboutText">
-        <p>
+        <span>
           I am an Environmental Scientist turned software developer ready to
           launch my programming career. In the environmental science field, I
           felt that the areas that I was most excited about involved technology.
           I have brought my passion, attention to detail, and teamwork mentality
           to increase the performance of your web designs.
-        </p>
-        <p></p>
-        <p className="secondaryText">
-          I have worked primarily in JavaScript, React, jQuery, SCSS, Node, and
-          Express.
-        </p>
+          <p></p>
+          <p className="secondaryText">
+            I have worked primarily in JavaScript, React, jQuery, SCSS, Node,
+            and Express.
+          </p>
+        </span>
 
         <img className="headshot" src={Images}></img>
       </div>
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,8 +1,49 @@
+import "./Portfolio.scss";
+import React from "react";
+import Images from "../Images/headshot.jpg";
+import { useState } from "react";
+
 function Portfolio() {
+  const [modal1, setModal1] = useState(false);
+  const setModal = () => {
+    setModal1(!modal1);
+    console.log(modal1);
+  };
   return (
     <div className="portfolio">
       <h2>Portfolio</h2>
       <h1>Choose an item to view the GitHub Repo</h1>
+
+      {setModal1 ? (
+        <button className="modal-btn" onClick={() => setModal()}>
+          Snackr
+        </button>
+      ) : (
+        <div className="modal-bg">
+          <div className="modal">
+            <h2>Snackr</h2>
+            <p>
+              Welcome To Snackr. The main advantage of this app is to discover
+              new snack ideas, through a curated list, to minimize time spent in
+              grocery and convenience stores. You may compile your own list of
+              saved snacks to keep new and exciting snack ideas at your
+              fingertips.
+            </p>
+            <p>Sprint: Two Week Solo Project</p>
+            <p>Node Express React PostGres Redux Saga Github SASS</p>
+            <a
+              target="_blank"
+              href="https://github.com/wkromar/prime-solo-project-2021"
+            ></a>
+            <button
+              className="modal-btn"
+              onClick={() => setModal(this.parent.parent)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
